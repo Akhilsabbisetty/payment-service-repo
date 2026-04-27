570ab89e60913ea4dac12bb8abda109dc6b2e99b
Akhil Sabbisetty <sabbisettyakhil22@gmail.com>
2026-04-27 16:01:28 +0530

    v1.0 of existing files

--- src/main/java/com/ascloud/PaymentApp.java
+++ src/main/java/com/ascloud/PaymentApp.java
@@ -3,20 +3,23 @@ package com.ascloud;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class PaymentApp {
-    public static void main(String[] args) {
-        SpringApplication.run(PaymentApp.class, args);
-    }
-}
-
-@Component
-class Consumer {
 
     @KafkaListener(topics = "orders")
     public void consume(String msg) {
-        System.out.println("Processing payment: " + msg);
+        System.out.println("Payment processed: " + msg);
+    }
+
+    @GetMapping("/payment")
+    public String status() {
+        return "Payment Service Running";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApp.class, args);
     }
 }
